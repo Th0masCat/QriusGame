67382598c62967133d46fdd40df2ea8c3eb92c6a
Th0masCat <catsaredogstoo@gmail.com>
2022-09-10 23:13:20 +0530

    "started dialogue system"

--- Assets/Scripts/TextboxPopup.cs
+++ Assets/Scripts/TextboxPopup.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
 public class TextboxPopup : MonoBehaviour
 {
     [SerializeField] GameObject textboxPopup;
+    public float offset;
+
+    [TextArea(2, 10)]
+    public string[] dialogue;
 
     private void OnTriggerEnter(Collider other)
     {
+        Vector3 position = new Vector3(transform.position.x, offset, transform.position.z);
         if (other.CompareTag("Player"))
         {
-            textboxPopup.SetActive(true);
+            Instantiate(textboxPopup, position , Quaternion.identity, transform);
         }
     }
 }
