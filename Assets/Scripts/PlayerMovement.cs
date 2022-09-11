ff20af4b83b51eba66150ba6dda216e2dbc19b51
Th0masCat <catsaredogstoo@gmail.com>
2022-09-11 11:41:12 +0530

    lappy reset ;-;

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private float rotationSpeed = 5.0f;
     private Animator playerAnimator;
