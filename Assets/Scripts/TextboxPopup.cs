50adac57fbcc816aef69c060feff164062e26af3
Th0masCat <catsaredogstoo@gmail.com>
2022-09-11 14:30:44 +0530

    Added dialogue box logic

--- Assets/Scripts/TextboxPopup.cs
+++ Assets/Scripts/TextboxPopup.cs
@@ -2,22 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-
 public class TextboxPopup : MonoBehaviour
 {
-    [SerializeField] GameObject textboxPopup;
-    public float offset;
+    private DialogueTrigger dialogueTrigger;
 
-    [TextArea(2, 10)]
-    public string[] dialogue;
+    private void Start()
+    {
+        dialogueTrigger = GetComponent<DialogueTrigger>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        Vector3 position = new Vector3(transform.position.x, offset, transform.position.z);
         if (other.CompareTag("Player"))
         {
-            Instantiate(textboxPopup, position , Quaternion.identity, transform);
+            dialogueTrigger.TriggerDialogue();
         }
     }
 }
