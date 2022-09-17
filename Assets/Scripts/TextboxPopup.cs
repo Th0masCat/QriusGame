3d052880a01ae7deb64deb530d16f88ddc9609b1
Th0masCat <catsaredogstoo@gmail.com>
2022-09-18 02:56:25 +0530

    Finished Environment

--- Assets/Scripts/TextboxPopup.cs
+++ Assets/Scripts/TextboxPopup.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class TextboxPopup : MonoBehaviour
 {
     private DialogueTrigger dialogueTrigger;
+    private Animator npcAnim;
 
     private void Start()
     {
         dialogueTrigger = GetComponent<DialogueTrigger>();
+        npcAnim = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            npcAnim.SetTrigger("BagAnim");
             dialogueTrigger.TriggerDialogue();
         }
     }
